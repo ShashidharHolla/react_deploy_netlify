aa65a2da1313a40ed7d44c4cd65fad6e51275110
ShashidharHolla <shashidharholla6@gmail.com>
2023-09-28 17:20:06 +0530

    First commit

---
+++ src/apiRequest.js
@@ -0,0 +1,13 @@
+const apiRequest= async (url="", optionObj=null, errMsg=null) => {
+    try{
+        const response= await fetch(url, optionObj);
+        if(!response.ok) throw Error('Please reload the app');
+    } catch (err){
+        errMsg= err.Message;
+    } finally {
+        return errMsg;
+    }
+
+}
+
+export default apiRequest;
