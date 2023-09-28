a8b8fef7af28ffdc54e0234b53d054ef55c2e9c7
ShashidharHolla <shashidharholla6@gmail.com>
2023-09-28 17:41:10 +0530

    URL modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState, useEffect } from 'react';
 import apiRequest from './apiRequest';
 
 function App() {
-  const API_URL=' https://sweet-monstera-99756b.netlify.app/';
+  const API_URL=' https://sweet-monstera-99756b.netlify.app/items';
 
   const [items, setItems]= useState([]);
   const [newItem, setNewItem]= useState('');
